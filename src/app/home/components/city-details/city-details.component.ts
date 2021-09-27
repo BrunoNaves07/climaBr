1927a17c23eb0b3a09f961daa1b6580b1fec31da
BrunoNaves07 <bruno.naves@estudante.ufla.br>
2021-09-27 20:31:39 -0300

    Implementação de componentes da tela 1

---
+++ src/app/home/components/city-details/city-details.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { IonTabButton } from '@ionic/angular';
+
+@Component({
+  selector: 'app-city-details',
+  templateUrl: './city-details.component.html',
+  styleUrls: ['./city-details.component.scss'],
+})
+export class CityDetailsComponent implements OnInit {
+
+  @Input() name: string;
+  @Input() state: string;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { CityDetailsComponent } from './components/city-details/city-details.component';
 
 
 @NgModule({
@@ -14,6 +15,9 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [
+    HomePage,
+    CityDetailsComponent
+  ]
 })
 export class HomePageModule {}
