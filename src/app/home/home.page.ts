956ca982672c0ce5498970feba94a24bcf8d970d
BrunoNaves07 <bruno.naves@estudante.ufla.br>
2021-10-05 20:51:36 -0300

    Implementação da camada Domain

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { City } from 'src/domain/entities/city';
+import { SearchCityService } from 'src/domain/services/search-city.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,20 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  cities: City[];
+  hasError: boolean = false;
+  errorMessage: string;
+
+  constructor(private readonly searchCity: SearchCityService) {}
+
+  async onSearch(query: string) {
+    try {
+      this.hasError = false;
+      this.cities = await this.searchCity.search(query);
+    } catch (error) {
+      this.hasError = true;
+      this.errorMessage = error.message;
+    }
+  }
 
 }
